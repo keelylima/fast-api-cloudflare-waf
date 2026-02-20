183a7d7f5538c1662942ead7330e7e2c59d116a6
Kelly Lima <kellylima@MacBook-Air-de-Kelly.local>
2026-02-19 21:32:26 -0300

    [Feat]Add route for get a ip in a ip list

--- final-project/app/routes/cloudflare_ip_lists.py
+++ final-project/app/routes/cloudflare_ip_lists.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from typing import List
-from app.services.cloudflare_ip_lists_service import list_ip_lists
+from app.services.cloudflare_ip_lists_service import list_ip_lists, list_ip_list_items
 from app.schemas.rules import (
     IPListResponse,
 )
@@ -14,3 +14,7 @@ router = APIRouter(
 @router.get("/{account_id}/ip-lists")
 async def get_ip_lists(account_id: str):
     return await list_ip_lists(account_id)
+
+@router.get("/{account_id}/ip-lists/{list_id}/items")
+async def get_ip_list_items(account_id: str, list_id: str):
+    return await list_ip_list_items(account_id, list_id)
--- final-project/app/services/cloudflare_ip_lists_service.py
+++ final-project/app/services/cloudflare_ip_lists_service.py
@@ -31,3 +31,20 @@ async def list_ip_lists(account_id: str):
 
     return response.json().get("result", [])
 
+
+async def list_ip_list_items(account_id: str, list_id: str):
+    headers = get_headers()
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            f"{settings.CLOUDFLARE_BASE_URL}/accounts/{account_id}/rules/lists/{list_id}/items",
+            headers=headers
+        )
+
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json()
+        )
+
+    return response.json().get("result", [])
