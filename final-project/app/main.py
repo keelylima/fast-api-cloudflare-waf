2fb0b74316f583ec8611434f3a90120e0729926d
Kelly Lima <kellylima@MacBook-Air-de-Kelly.local>
2026-02-20 22:05:30 -0300

    [Feat]Add customization title, description etc

--- final-project/app/main.py
+++ final-project/app/main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from app.routes.cloudflare import router as cloudflare_router
 from app.routes.cloudflare_ip_lists import router as cloudflare_ip_lists
 
-app = FastAPI()
+app = FastAPI(
+    title="Cloud Security Control API",
+    description="Internal Cloudflare governance API for WAF and IP list management.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
 
 app.include_router(cloudflare_router)
 app.include_router(cloudflare_ip_lists)
